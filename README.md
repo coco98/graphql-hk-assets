bcf21280697098eff21e410320e4e05061d578a6
Tanmai Gopal <tanmaig@hasura.io>
2019-12-04 12:06:34 +0530

    Adds init data and metadata

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 - Run Hasura + Postgres (use image tags from docker-compose.yaml)
 - Build the nodejs docker image (`cd nodejs-actions-service && docker build -t nodejs-actions-service .`)
 
-- Upload migrations + metadata from the `migrations/` folder
-
+- Upload migrations + metadata from the `init/` folder
+    - Run `init/create-table.sql` in the SQL window
+    - Upload the metadata from `init/metadata.json`
 
---
+++ init/create-table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name text NOT NULL,
+    email text NOT NULL
+);
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
